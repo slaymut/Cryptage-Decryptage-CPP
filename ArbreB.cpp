ea4d5315b84b78df790f2c83bec3fe20fa356268
Slaymut <slimani.salim.pro@gmail.com>
2020-11-09 20:24:17 +0100

    Added a few methods

--- ArbreB.cpp
+++ ArbreB.cpp
@@ -1,13 +1,56 @@
 #include "ArbreB.hpp"
+#include <iostream>
 
 template<class Type>
-c_Sommet<Type> *c_ArbreB<Type>::search() {
-    //Will complete later.
+void c_ArbreB<Type>::scanTree(c_Sommet<Type> *startNode) const{
+    //Left first because of standart tree reading.
+    if(startNode->left)
+        scanTree(startNode->left);
+
+    //Show current node value
+    if(startNode)
+        std::cout << "Value of current node: " << startNode->valueSommet << '\n';
+
+    //Right then
+    if(startNode->right)
+        scanTree(startNode->right);
 }
 
 template<class Type>
-void c_ArbreB<Type>::create(Type value){
-    //Will complete later.
+c_Sommet<Type> *c_ArbreB<Type>::search(Type &val) const {
+    //Take root
+    c_Sommet<Type> *current = root;
+
+    //While root not nullptr
+    while(current){
+        //If node is found, return it
+        if(current->valueSommet == val)
+            return current;
+
+        //Go left if value is
+        if(current->valueSommet > val)
+            current = current->right;
+        else
+            current = current->left;
+    }
+
+    return nullptr;
+}
+
+template<class Type>
+void c_ArbreB<Type>::createNode(const Type value){
+    //Allocate a new node
+    auto * newNode = new c_Sommet<Type>;
+
+    //Initialize left and right nodes
+    newNode->right = nullptr;
+    newNode->left = nullptr;
+
+    //Insert value
+    newNode->valueSommet = value;
+
+    //Insert node in tree
+    insert(newNode);
 }
 
 //Inserts a node in the tree.
@@ -31,10 +74,19 @@ void c_ArbreB<Type>::insert(c_Sommet<Type> *noodle){
             current = current->right;
     }
 
-    if(noodle->valeur < previous->valeur)
-        previous->gauche = noodle;
+    if(noodle->valueSommet < previous->valueSommet)
+        previous->left = noodle;
     else
-        previous->droite = noodle;
+        previous->right = noodle;
 }
 
-//UwU
+//Deletes a node in the tree.
+template<class Type>
+void c_ArbreB<Type>::m_delete(c_Sommet<Type> *node){
+    if (node==nullptr) return;
+
+    m_delete(node->right);
+    m_delete(node->left);
+
+    delete node;
+}
--- ArbreB.hpp
+++ ArbreB.hpp
@@ -7,16 +7,19 @@ template <class Type>
 class c_ArbreB{
 private:
     c_Sommet<Type> * root;
-    void insert(c_Sommet<Type> *Noodle);
+    void insert(c_Sommet<Type> *node);
+    void scanTree(c_Sommet<Type> *node) const;
+
 
 public:
     //Constructors
     c_ArbreB(): root(nullptr) {}
     explicit c_ArbreB(Type const &value): root(value) {}
 
+    void m_delete(c_Sommet<Type> *node);
     c_Sommet<Type>* getRoot(){return root;}
-    c_Sommet<Type>* search();
-    void create(Type value);
+    c_Sommet<Type>* search(Type &val) const;
+    void createNode(Type value);
 
     //Destructor
     ~c_ArbreB(){
